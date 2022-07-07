4d8becc68ac110231ef9d1fe8189265da564ffa1
Ankit Sahu <sahuankit010@Ankits-MacBook-Pro.local>
2022-07-06 22:01:55 -0500

    adding dependencies using npm

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,10 @@
-
+const validator = require('validator')
 const print = require('./notes')
+
 print('Hi I am Notes.')
+
+// console.log(validator.isEmail('abc@xyz.com'))
+// console.log(validator.isEmail('@xyz.com'))
+
+console.log(validator.isURL('xyz.com'))
+console.log(validator.isURL('////.com'))
