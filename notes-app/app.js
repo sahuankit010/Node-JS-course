168f0792ce56694720dd7fc73265902a40a46995
Ankit Sahu <sahuankit010@Ankits-MBP.lan>
2022-07-07 03:16:02 -0500

    nodemon and chalk addition

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,6 @@
+
 const validator = require('validator')
+const chalk = require('chalk')
 const print = require('./notes')
 
 print('Hi I am Notes.')
@@ -6,5 +8,7 @@ print('Hi I am Notes.')
 // console.log(validator.isEmail('abc@xyz.com'))
 // console.log(validator.isEmail('@xyz.com'))
 
-console.log(validator.isURL('xyz.com'))
-console.log(validator.isURL('////.com'))
+// console.log(validator.isURL('xyz.com'))
+// console.log(validator.isURL('////.com'))
+
+console.log(chalk.inverse.blue.bold('Success!'))
