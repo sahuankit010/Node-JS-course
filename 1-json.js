578e84a56fad218c85a99a9c7687b8e74f8075ea
Ankit Sahu <sahuankit010@Ankits-MBP.lan>
2022-07-08 03:12:59 -0500

    json-file added

---
+++ 1-json.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+user.name="Ankit"
+user.age = 26
+const userJSON= JSON.stringify(user)
+fs.writeFileSync('1-json.json',userJSON)
