28c71a2e01f96cabe79506b28a61db3f88ece8d6
Ankit Sahu <sahuankit010@Ankits-MBP.lan>
2022-07-08 02:30:56 -0500

    yargs file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,32 @@
 
 const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 const print = require('./notes')
 
-print('Hi I am Notes.')
+yargs.command({
+    command: 'add',
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title:"+ argv.title)
+        console.log("Body:"+ argv.body)
+    }
+})
+yargs.parse()
+// console.log(yargs.argv)
+// print('Hi I am Notes.')
 
 // console.log(validator.isEmail('abc@xyz.com'))
 // console.log(validator.isEmail('@xyz.com'))
@@ -11,4 +34,18 @@ print('Hi I am Notes.')
 // console.log(validator.isURL('xyz.com'))
 // console.log(validator.isURL('////.com'))
 
-console.log(chalk.inverse.blue.bold('Success!'))
+// console.log(chalk.inverse.blue.bold('Success!'))
+
+// // console.log(process.argv)
+
+// for(i in process.argv){
+//     console.log(process.argv[i])
+// }
+
+// const command = process.argv[2]
+
+// if(command === 'add'){
+//     console.log('Adding note!')
+// } else if(command === 'remove'){
+//     console.log('Removing note!')
+// }
