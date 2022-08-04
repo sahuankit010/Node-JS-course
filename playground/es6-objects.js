0a3664240254390ea04da996fba8cb8a5692564d
sahuankit010 <sahu.ankit010@gmail.com>
2022-08-03 21:56:38 -0500

    ES6-objects

---
+++ playground/es6-objects.js
@@ -0,0 +1,30 @@
+const name = 'Ankit'
+const userAge = 26
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Dallas'
+}
+
+console.log(user)
+
+const product = {
+    label: "Red Notebook",
+    price: 1.5,
+    stock: 201,
+    salePrice: null,
+    rating: 4
+}
+
+// const {label: productLabel, stock, rating = 5} = product
+
+// console.log(productLabel)
+// console.log(stock)
+// console.log(rating)
+
+const transaction = (type, {label, stock, salePrice}) => {
+    console.log(type, label, stock, salePrice)
+}
+
+transaction('order', product)
