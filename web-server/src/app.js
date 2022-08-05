ef897ef1f5f52df11671e4a79bae0200fb2443b1
sahuankit010 <sahu.ankit010@gmail.com>
2022-08-04 22:06:43 -0500

    expressjs started

---
+++ web-server/src/app.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Hot',
+        location: 'Dallas'
+    })
+})
+  
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
