b2b1b483661118bc735658a5ee964deaefd5c185
sahuankit010 <sahu.ankit010@gmail.com>
2022-08-05 21:42:45 -0500

    added the hbs with header and footer

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,44 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+hbs.registerPartials(partialsPath)
+
+
+//Setup static directory to use, which is not in use currently
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Ankit Sahu'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ankit Sahu'
+    })
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help', {
+        message: 'How can I help you?',
+        title: 'Help',
+        name: 'Ankit Sahu'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
