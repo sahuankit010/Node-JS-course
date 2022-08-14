a0c5da2f5eaf06152a63d2c6dfdd8f8d0bfbf588
sahuankit010 <sahu.ankit010@gmail.com>
2022-08-13 21:01:45 -0500

    Query Docs

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,51 +1,61 @@
 // CRUD Operations
 
-const {MongoClient, ObjectId} = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 // const MongoClient = mongodb.MongoClient
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectId()
-console.log(id.id)
-console.log(id.getTimestamp())
-MongoClient.connect(connectionURL, {useNewURLParser: true}, (error, client) => {
+// const id = new ObjectId()
+// console.log(id.id)
+// console.log(id.getTimestamp())
 
-    if(error){
+MongoClient.connect(connectionURL, { useNewURLParser: true }, (error, client) => {
+
+    if (error) {
         console.log(error)
         return console.log('Unable to connect with database')
     }
-
-    // console.log('Connected Successfully')
+    
     const db = client.db(databaseName)
-
-    // db.collection('users').insertOne({
-    //     name: 'Vinod',
-    //     age: 41
-    // }, (error, result) => {
+    
+    // db.collection('users').insertMany([
+    //     { name: 'Andrew', age: 26},
+    //     { name: 'Jen', age: 25 },
+    //     {name: 'Vikram', age: 26}
+    // ], (error, result) => {
     //     if(error){
-    //       return  console.log(error)
-    //     } 
-    //     console.log('Data')
-    //     console.log(result.insertedId)
-    //     console.log(result.acknowledged)
+    //         console.log('Error')
+    //         return console.log(error)
+    //     }
+
+    //     console.log(result.insertedCount)
+    // })
+    // db.collection('tasks').findOne({description: 'Read'}, (error, task) => {
+    //     if(error){
+    //         console.log('Error')
+    //         return console.log(error)
+    //     }
+    //     console.log(task)
     // })
 
-//     db.collection('tasks').insertMany([{
-//         description: 'Read',
-//         completed: true
-//     },{
-//         description: 'Code',
-//         completed: true
-//     },{
-//         description: 'Walk',
-//         completed: false
-//     }
-// ], (error, result) => {
-//     if(error){
-//         return console.log(error)
-//     } 
-//     console.log(result.acknowledged)
-//     console.log(result.insertedCount)
-// })
+    //Task
+
+    db.collection('tasks').findOne({_id: new ObjectId('62f84a0bc6b3542d357b98d0')}, (error, task) => {
+        if(error){
+            return console.log('Error' + error)
+        }
+        console.log(task)
+    })
+
+    
+
+    db.collection('tasks').find({completed: true}).toArray((error, tasks) => {
+        console.log('Tasks that are completed')
+        if(error){
+            return console.log('Error'+ error)
+        }
+
+        console.log(tasks)
+    })
 })
