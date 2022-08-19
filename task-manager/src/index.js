ec52666213042aa2d7b8a2d3a2280d98c155437a
sahuankit010 <sahu.ankit010@gmail.com>
2022-08-19 17:55:46 -0500

    get user/task api

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,6 +19,27 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    // console.log(req.params)
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user){
+            res.status(404).send()
+        }
+        res.status(200).send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Tasks(req.body)
     task.save().then(()=>{
@@ -26,6 +47,26 @@ app.post('/tasks', (req, res) => {
         
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Tasks.find({}).then((tasks) => {
+        res.status(200).send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Tasks.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
